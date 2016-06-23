12dd8411817256fe60c5a31a613784596f7ffe40
eivhyl <eivindh3@gmail.com>
2016-06-23 19:56:43 +0200

    Removed sensor routes

--- nodejs/app.js
+++ nodejs/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes');
-var sensorRoutes = require('./routes/sensor');
+//var sensorRoutes = require('./routes/sensor');
 
 var app = express();
 
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/sensor', sensorRoutes);
+//app.use('/sensor', sensorRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
